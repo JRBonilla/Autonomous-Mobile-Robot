97a5dbcbc9df98ddde280318b8f73e1ad913588d
Jonathan Bonilla <jonathanbonilla210@gmail.com>
2019-10-30 10:30:15 -0400

    Add files via upload

--- AMR/AMR.ino
+++ AMR/AMR.ino
@@ -1,1 +1,367 @@
+#include <ServoTimer2.h>
 
+const int MAX_DISTANCE = 11;
+const int MAX_SPEED    = 128;
+
+//======================================================
+// Motor variables
+//======================================================
+int left_motor_en    = 10;
+int left_motor_back  = 9;
+int left_motor_go    = 8;
+
+int right_motor_back = 7;
+int right_motor_go   = 6;
+int right_motor_en   = 5;
+
+//=========================================================
+// Ultrasonic edge detection and object avoidance variables
+//=========================================================
+int EdgeRightEcho = A1;  // Set Right Echo port
+int EdgeRightTrig = A0;  // Set Right Trig port
+int EdgeRightDist = 0;
+
+int EdgeLeftEcho = A5;   // Set Left Echo port
+int EdgeLeftTrig = A4;   // Set Left Trig port
+int EdgeLeftDist = 0;
+
+int AvoidEcho = A2;      // Set front facing left echo port
+int AvoidTrig = A3;      // Set front facing left trig port
+int AvoidDist = 0;
+
+//=========================================================
+// Servo variables
+//=========================================================
+int servoPort = 0;  // Servo port
+ServoTimer2 servo;  // Servo 
+
+//=========================================================
+// IR sensor variables
+//=========================================================
+const int IRSensorRight = 1;  // IR sensor right port (D1)
+const int IRSensorLeft  = 11; // IR sensor left port (D11)
+int SL, SR;                   // IR sensor states
+
+// Set button port
+int buttonPin = 13;
+
+// Set BUZZER port
+int beep = 12;
+
+// Initial setup code
+void setup() {
+  // Begin serial output
+  //Serial.begin(9600); // Do not uncomment this unless you want debugging and aren't using D0 and/or D1
+  
+  // Initialize left motor drive for output mode.
+  pinMode(left_motor_go,    OUTPUT);
+  pinMode(left_motor_back,  OUTPUT);
+  pinMode(right_motor_go,   OUTPUT);
+  pinMode(right_motor_back, OUTPUT);
+  pinMode(right_motor_en,   OUTPUT);
+  pinMode(left_motor_en,    OUTPUT);
+
+  pinMode(buttonPin, INPUT);       // Set button as input
+  //pinMode(beep, OUTPUT);         // Set buzzer as output
+
+  pinMode(EdgeRightEcho, INPUT);   // Set Echo port mode
+  pinMode(EdgeRightTrig, OUTPUT);  // Set Trig port mode
+
+  pinMode(EdgeLeftEcho, INPUT);    // Set Echo port mode
+  pinMode(EdgeLeftTrig, OUTPUT);   // Set Trig port mode
+
+  pinMode(AvoidEcho, INPUT);       // Set Echo port mode
+  pinMode(AvoidTrig, OUTPUT);      // Set Trig port mode
+
+  servo.attach(servoPort);
+
+  digitalWrite(buttonPin, HIGH);   // Initialize button
+  digitalWrite(beep, LOW);         // Set buzzer mute
+
+  pinMode(IRSensorRight, INPUT);   // Set right Line Walking IR sensor as input
+  pinMode(IRSensorLeft,  INPUT);   // Set left Line Walking IR sensor as input
+}
+
+// Move forward
+void run() {
+  digitalWrite(left_motor_en, HIGH);   // Left motor enable
+  analogWrite(left_motor_en, 130);
+  digitalWrite(right_motor_en, HIGH);  // Right motor enable
+
+  digitalWrite(right_motor_go, HIGH);  // Right motor go ahead
+  digitalWrite(right_motor_back, LOW);
+  analogWrite(right_motor_go, 130);    // PWM--Pulse Width Modulation (0~255). Right motor go speed is 130
+  analogWrite(right_motor_back, 0);
+
+  digitalWrite(left_motor_go, HIGH);   // Left motor go ahead
+  digitalWrite(left_motor_back, LOW);
+  analogWrite(left_motor_go, 130);     // PWM--Pulse Width Modulation (0~255). Left motor go speed is 130
+  analogWrite(left_motor_back, 0);
+}
+
+// Stop
+void brake() {
+  digitalWrite(right_motor_go,   LOW); // Stop the right motor
+  digitalWrite(right_motor_back, LOW);
+
+  digitalWrite(left_motor_go,    LOW); // Stop the left motor
+  digitalWrite(left_motor_back,  LOW);
+}
+
+// Turn left
+void left() {
+  digitalWrite(right_motor_go,  HIGH);    // Right motor go ahead
+  digitalWrite(right_motor_back, LOW);
+  analogWrite(right_motor_go, MAX_SPEED); // PWM--Pulse Width Modulation(0~255) control speed, right motor go speed is 255.
+  analogWrite(right_motor_back, 0);
+
+  digitalWrite(left_motor_go,   LOW);     // Left motor stop
+  digitalWrite(left_motor_back, LOW);
+  analogWrite(left_motor_go, 0);
+  analogWrite(left_motor_back, 0);
+}
+
+// Rotate left
+void spin_left(int time) {
+  digitalWrite(right_motor_go,  HIGH);     // Right motor go ahead
+  digitalWrite(right_motor_back, LOW);
+  analogWrite(right_motor_go, MAX_SPEED);  // PWM--Pulse Width Modulation(0~255) control speed, right motor go speed is 200
+  analogWrite(right_motor_back, 0);
+
+  digitalWrite(left_motor_go,   LOW);      // Left motor back off
+  digitalWrite(left_motor_back, HIGH);
+  analogWrite(left_motor_go, 0);
+  analogWrite(left_motor_back, MAX_SPEED); // PWM--Pulse Width Modulation(0~255) control speed, left motor back speed is 200.
+
+  delay(time * 100);
+}
+
+// Turn right
+void right() {
+  digitalWrite(right_motor_go,   LOW);   // Right motor stop
+  digitalWrite(right_motor_back, LOW);
+  analogWrite(right_motor_go,   0);
+  analogWrite(right_motor_back, 0);
+
+  digitalWrite(left_motor_go,  HIGH);    // Left motor go ahead
+  digitalWrite(left_motor_back, LOW);
+  analogWrite(left_motor_go, MAX_SPEED); // PWM--Pulse Width Modulation(0~255) control speed, left motor go speed is 230.
+  analogWrite(left_motor_back, 0);
+}
+
+// Rotate right
+void spin_right(int time) {
+  digitalWrite(right_motor_go,    LOW);     // Right motor back off
+  digitalWrite(right_motor_back, HIGH);
+  analogWrite(right_motor_go, 0);
+  analogWrite(right_motor_back, MAX_SPEED); // PWM--Pulse Width Modulation(0~255) control speed
+
+  digitalWrite(left_motor_go,  HIGH);       // Left motor go ahead
+  digitalWrite(left_motor_back, LOW);
+  analogWrite(left_motor_go, MAX_SPEED);    // PWM--Pulse Width Modulation(0~255) control speed
+  analogWrite(left_motor_back, 0);
+
+  delay(time * 100);
+}
+
+// Go backwards
+void back(int time) {
+  digitalWrite(right_motor_go,    LOW);     // Right motor back off
+  digitalWrite(right_motor_back, HIGH);
+  analogWrite(right_motor_go, 0);
+  analogWrite(right_motor_back, MAX_SPEED); // PWM--Pulse Width Modulation(0~255) control speed
+  analogWrite(right_motor_en, 200);
+
+  digitalWrite(left_motor_go,    LOW);      // Left motor back off
+  digitalWrite(left_motor_back, HIGH);
+  analogWrite(left_motor_go, 0);
+  analogWrite(left_motor_back, MAX_SPEED);  // PWM--Pulse Width Modulation(0~255) control speed
+
+  delay(time * 100);
+}
+
+// Right ultrasonic sensor distance test
+void Edge_right_distance_test() {
+  digitalWrite(EdgeRightTrig, LOW);               // Set trig port low level for 2μs
+  delayMicroseconds(2);
+  digitalWrite(EdgeRightTrig, HIGH);              // Set trig port high level for 10μs(at least 10μs)
+  delayMicroseconds(10);
+  digitalWrite(EdgeRightTrig, LOW);               // Set trig port low level
+  float Fdistance = pulseIn(EdgeRightEcho, HIGH); // Read echo port high level time(unit:μs)
+  Fdistance= Fdistance/58;                        // Distance(m) =(time(s) * 344(m/s)) / 2     /****** The speed of sound is 344m/s.*******/
+                                                  //  ==> 2*Distance(cm) = time(μs) * 0.0344(cm/μs)
+                                                  //  ==> Distance(cm) = time(μs) * 0.0172 = time(μs) / 58
+  Serial.print("Edge Right Distance:");           // Output Distance(cm)
+  Serial.println(Fdistance);                      // Display distance
+  EdgeRightDist = Fdistance;
+}  
+
+// Left ultrasonic sensor distance test
+void Edge_left_distance_test() {
+  digitalWrite(EdgeLeftTrig, LOW);               // Set trig port low level for 2μs
+  delayMicroseconds(2);
+  digitalWrite(EdgeLeftTrig, HIGH);              // Set trig port high level for 10μs(at least 10μs)
+  delayMicroseconds(10);
+  digitalWrite(EdgeLeftTrig, LOW);               // Set trig port low level
+  float Fdistance = pulseIn(EdgeLeftEcho, HIGH); // Read echo port high level time(unit:μs)
+  Fdistance= Fdistance/58;                       // Distance(m) =(time(s) * 344(m/s)) / 2     /****** The speed of sound is 344m/s.*******/
+                                                 //  ==> 2*Distance(cm) = time(μs) * 0.0344(cm/μs)
+                                                 //  ==> Distance(cm) = time(μs) * 0.0172 = time(μs) / 58
+  Serial.print("Edge Left Distance:");           // Output Distance(cm)
+  Serial.println(Fdistance);                     // Display distance
+  EdgeLeftDist = Fdistance;
+}  
+
+// Object avoidance sensor distance test
+void Avoid_distance_test() {
+  digitalWrite(AvoidTrig, LOW);               // Set trig port low level for 2μs
+  delayMicroseconds(2);
+  digitalWrite(AvoidTrig, HIGH);              // Set trig port high level for 10μs(at least 10μs)
+  delayMicroseconds(10);
+  digitalWrite(AvoidTrig, LOW);               // Set trig port low level
+  float Fdistance = pulseIn(AvoidEcho, HIGH); // Read echo port high level time(unit:μs)
+  Fdistance= Fdistance/58;                    // Distance(m) =(time(s) * 344(m/s)) / 2     /****** The speed of sound is 344m/s.*******/
+                                              //  ==> 2*Distance(cm) = time(μs) * 0.0344(cm/μs)
+                                              //  ==> Distance(cm) = time(μs) * 0.0172 = time(μs) / 58
+  Serial.print("Avoid Distance:");            // Output Distance(cm)
+  Serial.println(Fdistance);                  // Display distance
+  AvoidDist = Fdistance;
+}
+
+// Returns whether or not the object avoidance sensor detected an object
+boolean isObjectInFront(){
+    Avoid_distance_test(); // Check the avoidance sensor distance once.
+
+    int old = AvoidDist;   // Save the prior result and wait 250 ms before the next test.
+    delay(250);
+    
+    Avoid_distance_test(); // Check the avoidance sensor distance again.
+    
+    // If both checks are less than MAX_DISTANCE then there is something in front
+    if (AvoidDist < MAX_DISTANCE && old < MAX_DISTANCE ) {
+      return true;
+    }
+    return false;
+}
+
+void EdgeCheck(){
+    Edge_right_distance_test(); // Measuring right ultrasonic distance
+    Edge_left_distance_test();  // Measuring left ultrasonic distance
+    
+    if (EdgeRightDist > 20 && EdgeLeftDist > 20) {  // Both out of bounds
+      back(2);
+      spin_right(6);
+    }
+    if (EdgeRightDist < 20 && EdgeLeftDist > 20) {  // Right is out of bounds
+      back(2);
+      spin_left(6);
+    }
+    if (EdgeRightDist > 20 && EdgeLeftDist < 20) {  // Left is out of bounds
+      back(2);
+      spin_right(6);
+    }
+}
+
+// Check for button press
+void keyscan() {
+  int val = digitalRead(buttonPin);     // Reads the button, the level value assigns to val.
+  while (digitalRead(buttonPin)) {      // When the button is not pressed
+    val = digitalRead(buttonPin);
+  }
+  while (!digitalRead(buttonPin)) {     // When the button is pressed
+    delay(10); // delay 10ms
+    val = digitalRead(buttonPin);       // Read
+    if (val == LOW) {                   // Double check the button is pressed
+      digitalWrite(beep, HIGH);
+      delay(50);                        // Delay 50ms
+      while (!digitalRead(buttonPin)) { // Determine if button is released or not
+        digitalWrite(beep, LOW);        // Mute
+      }
+    }
+    else {
+      digitalWrite(beep, LOW);          // Mute
+    }
+  }
+}
+
+void loop() {
+  keyscan(); // Press the button to start
+ 
+  while (true) {
+    Avoid_distance_test();
+    
+    SR = digitalRead(IRSensorRight);
+    SL = digitalRead(IRSensorLeft);
+    
+    // Brake check
+    for (int i = 750; i <= 1925; i+= 375){
+      servo.write(i);
+      brake();
+      delay(500);
+      EdgeCheck();
+      if (isObjectInFront()) { // There is an object in front of the robot.
+        if (i <= 1500){
+          spin_left(5);
+        } else {
+          spin_right(5);
+        }
+        i=375;
+      }
+    }
+    
+    Edge_right_distance_test(); // Measuring right ultrasonic distance
+    Edge_left_distance_test();  // Measuring left ultrasonic distance
+
+    /*
+    if (S == HIGH && EdgeRightDist < 20 && EdgeLeftDist < 20) {
+      run();
+      delay(150);
+      brake();
+      delay(100);
+      
+      S = digitalRead(blackLineSensor);
+      if (S == LOW){
+        spin_right(4);
+        S = digitalRead(blackLineSensor);
+        if (S= LOW) {
+          
+        spin_left(8);
+        }
+      
+      }
+    }*/
+
+    // Black line following code
+    if (SL == LOW && SR == LOW) {       // No black lines detected
+      run();
+    }
+    else if (SL == LOW && SR == HIGH) { // Left sensor against white undersurface and right against black undersurface, the car left off track and need to adjust to the right.
+      right();
+    }
+    else if (SL == HIGH && SR == LOW) { // Right sensor against white undersurface and left against black undersurface, the car right off track and need to adjust to the left.
+      left();
+    }
+    
+    // Ultrasonic edge detection
+    if (EdgeRightDist < 20 && EdgeLeftDist < 20) {  // There is no obstacle, goes ahead.
+      run();
+      delay(150);
+      brake();
+      delay(100);
+    }
+    if (EdgeRightDist > 20 && EdgeLeftDist > 20) {  // Both out of bounds
+      back(2);
+      spin_right(6);
+    }
+    if (EdgeRightDist < 20 && EdgeLeftDist > 20) {  // Right is out of bounds
+      back(2);
+      spin_left(6);
+    }
+    if (EdgeRightDist > 20 && EdgeLeftDist < 20) {  // Left is out of bounds
+      back(2);
+      spin_right(6);  
+    }
+
+  }
+
+}
